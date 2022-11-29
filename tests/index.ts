4024e7065b0655b9a23d74565728205ded5c2940
hiroki osame <hiroki.osame@gmail.com>
2022-11-29 02:16:53 -0500

    fix: loading via module (#7)

--- tests/index.ts
+++ tests/index.ts
@@ -3,4 +3,7 @@ import { describe } from 'manten';
 describe('fs-require', ({ runTestSuite }) => {
 	runTestSuite(import('./specs/fs-require.js'));
 	runTestSuite(import('./specs/resolve.js'));
+
+	// eslint-disable-next-line import/extensions
+	runTestSuite(import('./specs/module.mjs'));
 });
